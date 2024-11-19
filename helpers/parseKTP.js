401aa53c3a287c6515df0757c476cf1b2b975cbd
Noel <hutasoitnoel@gmail.com>
2024-11-19 09:41:25 +0700

    fix ktp field naming

--- helpers/parseKTP.js
+++ helpers/parseKTP.js
@@ -4,17 +4,17 @@ module.exports = text => {
         kota: text.match(/JAKARTA\s+BARAT/i)?.[0]?.trim(),
         nik: text.match(/NIK\s*:\s*([\d]+)/i)?.[1]?.trim(),
         nama: text.match(/Nama\s*:\s*(.*)/i)?.[1]?.trim(),
-        tempatTanggalLahir: text
+        ttl: text
             .match(/Tempat\/Tgl Lahir\s*:\s*(.*)/i)?.[1]
             ?.trim(),
-        jenisKelamin: text.match(/Jenis Kelamin\s*:\s*(\w+)/i)?.[1]?.trim(),
-        golDarah: text.match(/Gol\. Darah\s*:\s*(\w+)/i)?.[1]?.trim(),
+        kelamin: text.match(/Jenis Kelamin\s*:\s*(\w+)/i)?.[1]?.trim(),
+        darah: text.match(/Gol\. Darah\s*:\s*(\w+)/i)?.[1]?.trim(),
         alamat: text.match(/Alamat\s*:\s*(.*)/i)?.[1]?.trim(),
-        rtRw: text.match(/RT\/RW\s*:\s*([\d\/]+)/i)?.[1]?.trim(),
-        kelDesa: text.match(/Kel\/Desa\s*:\s*(.*)/i)?.[1]?.trim(),
+        rtrw: text.match(/RTRW\s*:\s*([\d\/]+)/i)?.[1]?.trim(),
+        kelurahan: text.match(/Kel\/Desa\s*:\s*(.*)/i)?.[1]?.trim(),
         kecamatan: text.match(/Kecamatan\s*:\s*(.*)/i)?.[1]?.trim(),
         agama: text.match(/Agama\s*:\s*(.*)/i)?.[1]?.trim(),
-        statusPerkawinan: text
+        statusKawin: text
             .match(/Status Perkawinan\s*:\s*(.*)/i)?.[1]
             ?.trim(),
         pekerjaan: text.match(/Pekerjaan\s*:\s*(.*)/i)?.[1]?.trim(),
